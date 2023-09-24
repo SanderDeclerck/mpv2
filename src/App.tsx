8c2597c371809e8843cce9f8912e13af0606e47b
Thomas Stock <thomasstock1985@gmail.com>
2023-09-24 10:45:58 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { CreateVisitorPage } from "./pages/visitor/create";
+
+function App() {
+  return (
+    <div className="flex flex-row h-screen bg-gray-400  p-12">
+      <CreateVisitorPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/visitor/create/index.tsx
@@ -0,0 +1,26 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { CreateVisitorForm } from "./form";
+
+export function CreateVisitorPage() {
+  return (
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">something</TabsTrigger>
+        <TabsTrigger value="password">Something else here</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <CreateVisitorForm />
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Something else here</CardTitle>
+            <CardDescription>Change your password here. After saving, you'll be logged out.</CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">bla bla</CardContent>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  );
+}
