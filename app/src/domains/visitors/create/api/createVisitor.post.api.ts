535b8f4d7f4cbaf2910fc8baaa804f60dc6ec8d5
Thomas Stock <thomasstock1985@gmail.com>
2023-09-25 18:10:30 +0200

    create visitor post

--- app/src/domains/visitors/create/api/createVisitor.post.api.ts
+++ app/src/domains/visitors/create/api/createVisitor.post.api.ts
@@ -1,19 +1,32 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import z from "zod";
 
 export const profilesPostParams = z.object({
   profileId: z.number(),
   username: z.string(),
 });
-
 export type ProfilesPostParams = z.infer<typeof profilesPostParams>;
 
-export const postVisitor = async (params: ProfilesPostParams) => {
+export const profilesPostSuccessResponse = z.object({
+  id: z.number(),
+  profileId: z.number(),
+  username: z.string(),
+});
+export type ProfilesPostSuccessResponse = z.infer<typeof profilesPostSuccessResponse>;
+
+export const profilesPostErrorResponse = z.object({
+  error: z.enum(["usernameAlreadyExists", "noRights"]),
+});
+export type ProfilesPostErrorResponse = z.infer<typeof profilesPostErrorResponse>;
+
+export type ProfilesPostResponse = ProfilesPostSuccessResponse | ProfilesPostErrorResponse;
+
+export const postVisitor = async (params: ProfilesPostParams): Promise<ProfilesPostResponse> => {
   profilesPostParams.parse(params);
-  const response = await fetch(import.meta.env.VITE_API + "/profiles");
+  const response = await fetch(import.meta.env.VITE_API + "/profiles", { method: "POST", body: JSON.stringify(params) });
   const json = await response.json();
-  const validated = profilesGetResponse.parse(json);
+  const validated = profilesPostSuccessResponse.or(profilesPostErrorResponse).parse(json);
   return validated;
 };
 
-export const useProfiles = () => useQuery({ queryKey: ["profiles"], queryFn: getProfiles });
+export const useCreateVisitor = () => useMutation({ mutationFn: postVisitor });
