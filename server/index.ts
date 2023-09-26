e96ac682d1e188fcbd1cb95aa2730d2d32d3e445
Thomas Stock <thomasstock1985@gmail.com>
2023-09-26 19:41:33 +0200

    .

--- server/index.ts
+++ server/index.ts
@@ -30,36 +30,6 @@ app.get("/setState", (req, res) => {
   res.sendStatus(201);
 });
 
-// app.get("/tasks", (_, res) => {
-//   setTimeout(() => {
-//     return res.json(getTasks());
-//   }, 3000);
-// });
-
-// app.get("/tasks/:id/complete", (req, res) => {
-//   const id = req.params.id;
-//   tasks = tasks.filter((_) => _.id.toString() != id);
-//   console.log(tasks);
-//   setTimeout(() => {
-//     res.json(tasks);
-//   }, 1000);
-// });
-
-// app.post("/tasks/create", (req, res) => {
-//   const id = generateId();
-//   tasks = [
-//     ...tasks,
-//     {
-//       id: generateId(),
-//       label: "New task " + id,
-//     },
-//   ];
-//   console.log(tasks);
-//   setTimeout(() => {
-//     res.json(tasks);
-//   }, 2000);
-// });
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
