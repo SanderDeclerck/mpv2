71b158f7d4241687cd4741dc7d98e207a94f927d
Thomas Stock <thomasstock1985@gmail.com>
2023-09-30 11:44:01 +0200

    strong types mock

--- app/src/server/registerMock.ts
+++ app/src/server/registerMock.ts
@@ -2,6 +2,9 @@ import { RequestHandler } from "express";
 import { app } from "./app";
 import { PostFn } from "@/lib/createApi";
 
-export const registerMock = <B, R>(apiCall: PostFn<B, R>, handler: RequestHandler) => {
+export const registerMock = <Body, Response>(
+  apiCall: PostFn<Body, Response>,
+  handler: RequestHandler<undefined, Response, Body>
+) => {
   app.post(apiCall.url, handler);
 };
